4d40067d2fe01454ceb9d026c0396164a9142166
ramchanderpeddada <ramchandpeddada@gmail.com>
2022-01-02 19:26:44 +0530

    changes in functions

--- src/Components/Dialogue.jsx
+++ src/Components/Dialogue.jsx
@@ -1,38 +1,46 @@
 import {
-	Dialog,
-	DialogContent,
-	DialogContentText,
-	DialogActions,
-	Button,
-} from '@material-ui/core'
-import React, {useEffect} from 'react'
-import {deleteUser} from '../Service/Link'
+  Dialog,
+  DialogContent,
+  DialogContentText,
+  DialogActions,
+  Button,
+} from "@material-ui/core";
+import React, { useEffect } from "react";
+import { deleteUser, getUser } from "../Service/Link";
 
-const Dialogue = ({open, closeClickHandler}) => {
-	useEffect(() => {
-		getAllUsers()
-	}, [])
+const Dialogue = ({ open, closeClickHandler, setUsers }) => {
+  useEffect(() => {
+    getAllUsers();
+  }, []);
 
-	const deleteFunction = async (id) => {
-		await deleteUser(id)
-		getAllUsers()
-	}
+  const deleteFunction = async (id) => {
+    await deleteUser(id);
+    getAllUsers();
+  };
+  const getAllUsers = async () => {
+    const response = await getUser();
+    setUsers(response.data);
+  };
 
-	return (
-		<div>
-			<Dialog open={open} onClose={closeClickHandler}>
-				<DialogContent>
-					<DialogContentText id='alert-dialog-description'>
-						Are you sure you want to delete?
-					</DialogContentText>
-				</DialogContent>
-				<DialogActions>
-					<Button onClick={deleteFunction}>Yes</Button>
-					<Button>No</Button>
-				</DialogActions>
-			</Dialog>
-		</div>
-	)
-}
+  return (
+    <div>
+      <Dialog open={open} onClose={closeClickHandler}>
+        <DialogContent>
+          <DialogContentText id="alert-dialog-description">
+            Are you sure you want to delete?
+          </DialogContentText>
+        </DialogContent>
+        <DialogActions>
+          <Button onClick={deleteFunction} color="primary" variant="contained">
+            Yes
+          </Button>
+          <Button color="secondary" variant="contained">
+            No
+          </Button>
+        </DialogActions>
+      </Dialog>
+    </div>
+  );
+};
 
-export default Dialogue
+export default Dialogue;
--- src/Components/Users.jsx
+++ src/Components/Users.jsx
@@ -1,107 +1,104 @@
-import React, {useEffect, useState} from 'react'
+import React, { useEffect, useState } from "react";
 import {
-	makeStyles,
-	Table,
-	TableBody,
-	TableCell,
-	TableHead,
-	TableRow,
-	Typography,
-	Button,
-} from '@material-ui/core'
-import {Link} from 'react-router-dom'
-import {deleteUser, getUser} from '../Service/Link'
-import Dialogue from './Dialogue'
+  makeStyles,
+  Table,
+  TableBody,
+  TableCell,
+  TableHead,
+  TableRow,
+  Typography,
+  Button,
+} from "@material-ui/core";
+import { Link } from "react-router-dom";
+import { deleteUser, getUser } from "../Service/Link";
+import Dialogue from "./Dialogue";
 const useStyle = makeStyles({
-	table: {
-		width: '90%',
-		margin: '60px 0 0 50px',
-	},
-	tableHead: {
-		'& > *': {
-			backgroundColor: 'lightblue',
-		},
-	},
-})
+  table: {
+    width: "90%",
+    margin: "60px 0 0 50px",
+  },
+  tableHead: {
+    "& > *": {
+      backgroundColor: "lightblue",
+    },
+  },
+});
 const Users = () => {
-	const [users, setUsers] = useState([])
+  const [users, setUsers] = useState([]);
 
-	const [open, setOpen] = useState(false)
+  const [open, setOpen] = useState(false);
 
-	const openClickHandler = () => {
-		setOpen(true)
-	}
-	const closeClickHandler = () => {
-		setOpen(false)
-	}
-	const styleChanger = useStyle()
+  const openClickHandler = () => {
+    setOpen(true);
+  };
+  const closeClickHandler = () => {
+    setOpen(false);
+  };
+  const styleChanger = useStyle();
 
-	
+  // const deleteUserData = async (id) => {
+  // 	setOpen(true)
 
-	// const deleteUserData = async (id) => {
-	// 	setOpen(true)
+  // 	// if (confirmBox === true) {
+  // 	//
+  // 	// }
+  // }
 
-	// 	// if (confirmBox === true) {
-	// 	// 
-	// 	// }
-	// }
+  return (
+    <Table className={styleChanger.table}>
+      <TableHead>
+        <Typography variant="h4">Users</Typography>
+        <TableRow className={styleChanger.tableHead}>
+          <TableCell>Id</TableCell>
+          <TableCell>Name</TableCell>
+          <TableCell>Username</TableCell>
+          <TableCell>Email</TableCell>
+          <TableCell>Phone</TableCell>
+          <TableCell></TableCell>
+        </TableRow>
+      </TableHead>
+      <TableBody>
+        {users.map((user) => (
+          <TableRow>
+            <TableCell>{user.id}</TableCell>
+            <TableCell>{user.name}</TableCell>
+            <TableCell>{user.username}</TableCell>
+            <TableCell>{user.email}</TableCell>
+            <TableCell>{user.phone}</TableCell>
+            <TableCell>
+              <Typography
+                style={{
+                  textAlign: "right",
+                }}
+              >
+                <Button
+                  variant="contained"
+                  color="primary"
+                  style={{ marginRight: 10 }}
+                  to={`/edit/${user.id}`}
+                  component={Link}
+                >
+                  Edit
+                </Button>
+                <Button
+                  color="secondary"
+                  variant="contained"
+                  onClick={() => openClickHandler(user.id)}
+                >
+                  Delete
+                </Button>
+              </Typography>
+            </TableCell>
+          </TableRow>
+        ))}
+      </TableBody>
+      <Dialogue
+        open={open}
+        closeClickHandler={closeClickHandler}
+        setUsers={setUsers}
+      />
+    </Table>
+  );
+};
 
-	const getAllUsers = async () => {
-		const response = await getUser()
-		setUsers(response.data)
-	}
-
-	return (
-		<Table className={styleChanger.table}>
-			<TableHead>
-				<Typography variant='h4'>Users</Typography>
-				<TableRow className={styleChanger.tableHead}>
-					<TableCell>Id</TableCell>
-					<TableCell>Name</TableCell>
-					<TableCell>Username</TableCell>
-					<TableCell>Email</TableCell>
-					<TableCell>Phone</TableCell>
-					<TableCell></TableCell>
-				</TableRow>
-			</TableHead>
-			<TableBody>
-				{users.map((user) => (
-					<TableRow>
-						<TableCell>{user.id}</TableCell>
-						<TableCell>{user.name}</TableCell>
-						<TableCell>{user.username}</TableCell>
-						<TableCell>{user.email}</TableCell>
-						<TableCell>{user.phone}</TableCell>
-						<TableCell>
-							<Typography
-								style={{
-									textAlign: 'right',
-								}}
-							>
-								<Button
-									variant='contained'
-									color='primary'
-									style={{marginRight: 10}}
-									to={`/edit/${user.id}`}
-									component={Link}
-								>
-									Edit
-								</Button>
-								<Button
-									color='secondary'
-									variant='contained'
-									onClick={() => openClickHandler(user.id)}
-								>
-									Delete
-								</Button>
-							</Typography>
-						</TableCell>
-					</TableRow>
-				))}
-			</TableBody>
-			<Dialogue open={open} closeClickHandler={closeClickHandler} />
-		</Table>
-	)
-}
-
-export default Users
+export default Users;
